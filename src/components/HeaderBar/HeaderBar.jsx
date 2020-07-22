b6e4e8a467312cf89524507856c380ee787b6195
thedevilx <aadithya794@gmail.com>
2020-07-23 04:09:19 +0530

    Changed Header Bar Component

--- src/components/HeaderBar/HeaderBar.jsx
+++ src/components/HeaderBar/HeaderBar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import '../HeaderBar/headerbar.min.scss';
 
 function HeaderBar() {
     return (
-        <div className="tpbr flex" style={{margin:'0', padding:'0', width: '100%', backgroundColor:'black', padding: '0', margin:'0'}}>
+        <div className="tpbr flex" style={{margin:'0', padding:'0', width: '100%', backgroundColor:'black'}}>
             <div className="container">
                 <div className="tpbr-inr">
                     <div className="row justify-content-between">
                         <div className="col-md-6 col-sm-12 col-lg-6">
                             <p itemprop="description"><i className="flaticon-error thm-clr"></i><strong>Crisis Watch:</strong> Covid-19 In India</p>
                         </div>
-                        <div className="col-md-6 col-sm-12 col-lg-6 text-right">
+                        <div className="col-md-6 col-sm-12 col-lg-6 txtrgt">
                             <ul className="tp-lnks">
                                 <li><i className="fa fa-gear"></i><a href="http://bengaluruveeraru.org/" title="" itemprop="url">BV</a></li>
                                 <li><i className="fa fa-id-card"></i><a href="#top" title="" itemprop="url">Careers</a></li>
