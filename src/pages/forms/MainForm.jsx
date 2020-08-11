a9fb6d21242293c9541574d635e708c5ff89f0ee
thedevilx <aadithya794@gmail.com>
2020-08-12 04:02:52 +0530

    Fixed Page Scroll Top on Reload

--- src/pages/forms/MainForm.jsx
+++ src/pages/forms/MainForm.jsx
@@ -2,24 +2,22 @@ import React from "react";
 import JotformEmbed from "react-jotform-embed";
 import HeaderBar from "../../components/HeaderBar/HeaderBar";
 import MenuBar from "../../components/MenuBar/MenuBar";
-import ResponsiveHeader from "../../components/ResponsiveHeader/ResponsiveHeader";
-import StickyHeader from "../../components/StickyHeader/StickyHeader";
 import Footer from "../../components/Footer/Footer";
 import "../forms/mainform.min.scss";
 
-function EventDetails8() {
-  
-  return (
-    
-    <main>
+  const EventDetails8 = () => {
+    React.useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
+
+    return (
+      <main>
         <HeaderBar></HeaderBar>
 
         <MenuBar></MenuBar>
-      <div className="container">
-        <div className="row">
-
+        <div className="container">
+          <div className="row"></div>
         </div>
-      </div>
         {/* <div className="containerx">
             <div className="container">
                 <div className="row">
@@ -27,13 +25,12 @@ function EventDetails8() {
                 </div>
             </div>
         </div> */}
-        
+
         <JotformEmbed src="https://form.jotform.com/letsbethechange.india/signup" />
 
         <Footer></Footer>
       </main>
- 
-  );
-}
+    );
+  };
 
 export default EventDetails8;
