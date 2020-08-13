427c58bf3849548ac8b10d599bbcb3e6534bdb8d
thedevilx <aadithya794@gmail.com>
2020-08-13 19:59:16 +0530

    Fixed Warnings in Funding Section

--- src/components/EventFunding/EventFunding.jsx
+++ src/components/EventFunding/EventFunding.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+//import { Switch, Route, Link } from "react-router-dom";
 
 function EventFunding() {
     return (
