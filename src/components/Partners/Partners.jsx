055b62833bbf088c33dae5469ae29e7c08fde0bd
thedevilx <aadithya794@gmail.com>
2020-08-16 03:46:21 +0530

    Changed Background of Partners Logo

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Switch, Route, Link } from "react-router-dom";
+import "../Partners/partners.min.scss"
 
 function Partners(props){
     return (
       <div className="col-md-4 col-sm-4 col-lg-4">
-        <div className="orgn-bx2">
+        <div className="orgn-bx2 bkgndwhite">
           <img
             src= {props.imgSrc}
             alt="org-img2-1.png"
