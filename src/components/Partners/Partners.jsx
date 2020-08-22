fcb2e516e3217d91917e52f732bcdc1e57b16382
Karthik Ravishankar <br.karthik1@gmail.com>
2020-08-22 19:15:12 +0530

    Updated Opacity of Partner Images and also removed the grayscale

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Switch, Route, Link } from "react-router-dom";
 import "../Partners/partners.min.scss"
 
-function Partners(props){
-    return (
-      <div className="col-md-4 col-sm-4 col-lg-4">
-        <div className="orgn-bx2 bkgndwhite">
-          <img
-            src= {props.imgSrc}
-            alt="org-img2-1.png"
-            itemprop="image"
-          />
-        </div>
+function Partners(props) {
+  return (
+    <div className="col-md-4 col-sm-4 col-lg-4">
+      <div className="orgn-bx2 bkgndwhite part-img">
+        <img
+          src={props.imgSrc}
+          alt="org-img2-1.png"
+          itemprop="image"
+        />
       </div>
-    );
+    </div>
+  );
 }
 
 export default Partners;
