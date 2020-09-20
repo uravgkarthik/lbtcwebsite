6361168a76eadba108808c76047c60c1dd2d050b
Karthik Ravishankar <br.karthik1@gmail.com>
2020-09-20 23:47:13 +0530

    Fixed Error in Teams

--- src/components/OurTeam/ProfileCard.jsx
+++ src/components/OurTeam/ProfileCard.jsx
@@ -1,59 +1,59 @@
 import React from 'react';
 
-const ProfileCard=(props)=> {
-    const {person} = props     
-    
-    return(
+const ProfileCard = (props) => {
+    const { person } = props
+
+    return (
         <div>
             <div className="tem-bx">
                 <div
                     className="tem-thmb"
                     style={{
                         backgroundImage:
-                            "url("+person.photo+")"
+                            "url(" + person.photo + ")"
                     }}
                 >
                     <div className="tem-scl">
-                    {person.linkedinlink && 
-                        <a
-                            className="facebook"
-                            href={person.linkedinlink}
-                            title="Linkedin"
-                            itemprop="url"
-                            target="_blank" rel="noopener noreferrer"
-                        >
-                            <i className="fa fa-linkedin"></i>
-                        </a>
-                    }
-                    {person.facebooklink && 
-                        <a
-                            className="pinterest"
-                            href={person.facebooklink}
-                            title="Instagram"
-                            itemprop="url"
-                            target="_blank" rel="noopener noreferrer"
-                        >
-                            <i className="fa fa-instagram"></i>
-                        </a>
-                    }
-                    {person.twitterlink &&
-                        <a
-                            className="twitter"
-                            href={person.twitterlink}
-                            title="Email"
-                            itemprop="url"
-                            target="_blank" rel="noopener noreferrer"
-                        >
-                            <i className="fa fa-envelope"></i>
-                        </a>
-                    }
+                        {person.linkedinlink &&
+                            <a
+                                className="facebook"
+                                href={person.linkedinlink}
+                                title="Linkedin"
+                                itemprop="url"
+                                target="_blank" rel="noopener noreferrer"
+                            >
+                                <i className="fa fa-linkedin"></i>
+                            </a>
+                        }
+                        {person.facebooklink &&
+                            <a
+                                className="pinterest"
+                                href={person.facebooklink}
+                                title="Instagram"
+                                itemprop="url"
+                                target="_blank" rel="noopener noreferrer"
+                            >
+                                <i className="fa fa-instagram"></i>
+                            </a>
+                        }
+                        {person.twitterlink &&
+                            <a
+                                className="twitter"
+                                href={person.twitterlink}
+                                title="Email"
+                                itemprop="url"
+                                target="_blank" rel="noopener noreferrer"
+                            >
+                                <i className="fa fa-envelope"></i>
+                            </a>
+                        }
                     </div>
                 </div>
                 <div className="tem-inf">
                     <h4 itemprop="headline">
-                        <a href="team-detail.html" title="" itemprop="url">
-                            {person.name}
-                        </a>
+
+                        {person.name}
+
                     </h4>
                     <span>{person.designation}</span>
                 </div>
